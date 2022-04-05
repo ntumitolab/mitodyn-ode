3a87f9df244c90fc4320f596d39b14132bd98736
Wen-Wei Tseng <sosiristseng@gmail.com>
2022-04-05 12:50:34 +0800

    no sysimage for now

--- build_kernel.jl
+++ build_kernel.jl
@@ -1,21 +1,21 @@
 # Adapted from https://github.com/terasakisatoshi/sysimage_creator/
 import Pkg
 
-Pkg.add(["PackageCompiler", "IJulia"])
+Pkg.add(["IJulia"])
 
-sysimage_path = joinpath(@__DIR__, "sysimage.so")
+# sysimage_path = joinpath(@__DIR__, "sysimage.so")
 
-@info "SysImage path: " sysimage_path
+# @info "SysImage path: " sysimage_path
 
-using PackageCompiler
+# using PackageCompiler
 
 # Do not include too many packages because memory on CI machines is limited
-PackageCompiler.create_sysimage(
-    ["DifferentialEquations"];
-    project=".",
-    sysimage_path=sysimage_path,
-    cpu_target=PackageCompiler.default_app_cpu_target()
-)
+# PackageCompiler.create_sysimage(
+#     ["DifferentialEquations"];
+#     project=".",
+#     sysimage_path=sysimage_path,
+#     cpu_target=PackageCompiler.default_app_cpu_target()
+# )
 
 nthreads = Sys.CPU_THREADS
 
@@ -23,11 +23,11 @@ nthreads = Sys.CPU_THREADS
 
 using IJulia
 kernelpath = IJulia.installkernel("Julia-sys-$(nthreads)-threads",
-    "--project=@.", "--sysimage=$(sysimage_path)";
+    "--project=@."; # "--sysimage=$(sysimage_path)";
     specname="julia", env=Dict("JULIA_NUM_THREADS" => "$(nthreads)",)
 )
 
-@info "IJulia kernal path: " kernelpath
+@info "IJulia kernel path: " kernelpath
 
-Pkg.rm("PackageCompiler")
-Pkg.gc()
+# Pkg.rm("PackageCompiler")
+# Pkg.gc()
