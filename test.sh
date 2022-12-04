09eff7af4a98fb6e93f64c1044d20b7c84becc61
Wen-Wei Tseng <sosiristseng@gmail.com>
2022-12-04 15:33:39 +0800

    rm test bash script

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-julia --color=yes --project=@. -e 'import Pkg; Pkg.instantiate(); Pkg.precompile()'
-
-parallel --joblog /tmp/log -j8 jupyter nbconvert --to notebook --ExecutePreprocessor.timeout=600 --execute --inplace {} ::: docs/*.ipynb
-cat /tmp/log
-
-parallel -j8 jupyter nbconvert --clear-output --inplace {} ::: docs/*.ipynb
