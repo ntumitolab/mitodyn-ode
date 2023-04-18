b5f7498fc603f93b03f37610b732152267d46773
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-04-18 17:19:28 +0800

    wip

--- src/MitochondrialDynamics.jl
+++ src/MitochondrialDynamics.jl
@@ -8,7 +8,7 @@ include("utils.jl")
 include("rates.jl")
 
 
-
+# Convineince function
 hil(x, k=one(x)) = x / (x + k)
 hil(x, k, n) = hil(x^n, k^n)
 
@@ -32,13 +32,15 @@ function make_model(;
     simplify=true,
     caceq=cac_eq_atp(),
     glceq=glc_eq(),
-    rpdh=1,
-    retc=1,
-    rf1=1,
-    rhleak=1,
-    j_ffa=0,
     gk_atp_stoich::Int=2
 )
+    @constants C_MIT=1.812μM/mV # Mitochondrial membrane capacitance
+    @constants F_M = 3E-4       # Frasction of free Ca in mitochondria
+    @constants F_I = 0.01       # Fraction of free Ca in cytosol
+    @constants V_I = 0.53       # Relative cytoplasmic volume
+    @constants V_MT = 0.06      # Relative mitochondrial volume
+    @constants V_MTX = 0.0144   # Relative mitochondrial matrix volume
+
     @variables t
 
     # Adenylate kinase (AdK)
@@ -62,8 +64,8 @@ function make_model(;
     ldheq = J_LDH(t) ~ VmaxLDH * hil(Pyr, KpyrLDH) * hil(NADH_c, NAD_c * KnadhLDH)
 
     # Pyruvaye dehydrogenase (PDH)
-    @variables J_PDH(t) J_DH(t) J_CAC(t) J_FFA(t) NAD_m(t) NADH_m(t) Ca_m(t)
-    @parameters VmaxPDH=300μM*Hz KpyrPDH=47.5μM KnadPDH=81 U1PDH=1.5 U2PDH=1.1 KcaPDH=0.05μM
+    @variables J_PDH(t) J_DH(t) J_CAC(t) NAD_m(t) NADH_m(t) Ca_m(t)
+    @parameters VmaxPDH=300μM*Hz KpyrPDH=47.5μM KnadPDH=81 U1PDH=1.5 U2PDH=1.1 KcaPDH=0.05μM J_FFA=0μM*Hz
     pdheq = let
         c = (hil(KcaPDH, Ca_m))^2
         fpCa = hil(1, U2PDH * (1 + U1PDH * c))
@@ -125,55 +127,49 @@ function make_model(;
     @parameters ΣAc=4.5mM Σn_c=2mM Σn_m=2.2mM
 
     # Fission-fusion rates
-    @variables (x(t))[1:3] degavg(t)
-    @parameters kfiss1=inv(10minute) kfuse1=kfiss1 kfiss2=1.5kfiss1 kfuse2=0.5kfuse1
+    @variables (x(t))[1:3] degavg(t) v1(t) v2(t)
+    @parameters kfiss1=inv(10minute) kfuse1=kfiss1 kfiss2=1.5*kfiss1 kfuse2=0.5*kfuse1
 
     D = Differential(t)
+    # Reaction equations
     eqs = [
-        # Reactions
-        J_GK ~ j_gk(ATP_c, Glc, VmaxGK, KatpGK, KglcGK, nGK),
-        J_GPD ~ j_gpd(G3P, NAD_c, NADH_c, ADP_c, VmaxGPD, Kg3pGPD, KnadGPD, KadpGPD),
-        J_LDH ~ j_ldh(Pyr, NAD_c, NADH_c, VmaxLDH, KpyrLDH, KnadhLDH),
-        J_ADK ~ j_adk(ATP_c, ADP_c, AMP_c, kfAK, kEqAK),
-        J_PDH ~ rPDH * j_pdh(Pyr, NAD_m, NADH_m, Ca_m, VmaxPDH, KpyrPDH, KnadPDH, U1PDH, U2PDH, KcaPDH),
-        J_CAC ~ J_PDH + j_ffa,
+        caceq,
+        glceq,
+        adkeq,
+        gkeq,
+        gpdeq,
+        ldheq,
+        pdheq,
+        hreq,
+        hkeq,
+        hfeq,
+        mcueq,
+        nclxeq,
+        nadhteq,
+        J_CAC ~ J_PDH + J_FFA,
         J_DH ~ 4.6 * J_CAC,
-        J_HR ~ rETC * j_hr(NADH_m, ΔΨm, VmaxETC, KnadhETC, KaETC, KbETC),
-        J_O2 ~ 0.1 * J_HR,
-        J_HL ~  rHL * j_hl(ΔΨm, pHleak, kvHleak),
-        J_HF ~ rF1 * j_hf(ADP_c, Ca_m, ΔΨm, VmaxF1, KadpF1, KvF1, KcaF1, FmgadpF1),
+        J_O2 ~ J_HR / 10,
         J_ANT ~ J_HF / 3,
-        J_MCU ~ j_uni(Ca_m, Ca_c, ΔΨm, PcaMCU),
-        J_NCLX ~ j_nclx(Ca_m, Ca_c, Na_m, Na_c, VmaxNCLX, KcaNCLX, KnaNCLX),
-        J_NADHT ~ j_nadht(NAD_c, NADH_c, NAD_m, NADH_m, VmaxNADHT, Ktn_c, Ktn_m),
-        v[1] ~ kfuse1 * J_ANT / J_HL * x[1] * x[1] - kfiss1 * x[2],
-        v[2] ~ kfuse2 * J_ANT / J_HL * x[1] * x[2] - kfiss2 * x[3],
-        Glc ~ glcrhs,
-        Ca_c ~ cacrhs,
-        rPDH ~ rpdh,
-        rETC ~ retc,
-        rF1 ~ rf1,
-        rHL ~ rhleak,
+        v1 ~ kfuse1 * J_ANT / J_HL * x[1] * x[1] - kfiss1 * x[2],
+        v2 ~ kfuse2 * J_ANT / J_HL * x[1] * x[2] - kfiss2 * x[3],
         # Conservation relationships
         ΣAc ~ ATP_c + ADP_c + AMP_c,
         Σn_c ~ NADH_c + NAD_c,
         Σn_m ~ NADH_m + NAD_m,
         1 ~ x[1] + 2x[2] + 3x[3],
         # Observables
-        x13r ~ x[1] / x[3],
         degavg ~ (x[1] + 2x[2] + 3x[3]) / (x[1] + x[2] + x[3]),
-        # AMPKactivity ~ hill(AMP_c / ATP_c, kAMPK),
         # State variables
-        D(NADH_m) ~ iVmtx * (J_DH + J_NADHT - J_O2) - kNADHm * NADH_m,
+        V_MTX * D(NADH_m) ~ J_DH + J_NADHT - J_O2 - V_MTX * kNADHm * NADH_m,
         # D(NAD_m) ~ # Conserved
-        D(NADH_c) ~ iVi * (J_GPD - J_NADHT - J_LDH) - kNADHc * NADH_c,
+        V_I * D(NADH_c) ~ J_GPD - J_NADHT - J_LDH - V_I * kNADHc * NADH_c,
         # D(NAD_c) ~ # Conserved
-        D(Ca_m) ~ iVmtx * F_M * (J_MCU - J_NCLX),
-        D(ΔΨm) ~ iCmt * (J_HR - J_HF - J_HL - J_ANT - 2J_MCU),
-        D(G3P) ~ iVi * (2J_GK - J_GPD) - kG3P * G3P,
-        D(Pyr) ~ iVimtx * (J_GPD - J_PDH - J_LDH) - kPyr * Pyr,
-        D(ATP_c) ~ iVi * (-gk_atp_stoich * J_GK + 2J_GPD + J_ANT + J_ADK) - ATP_c * (kATP + kATPCa * Ca_c),
-        D(AMP_c) ~ iVi * J_ADK,
+        V_MTX * D(Ca_m) ~ F_M * (J_MCU - J_NCLX),
+        C_MIT * D(ΔΨm) ~ J_HR - J_HF - J_HL - J_ANT - 2 * J_MCU,
+        V_I * D(G3P) ~ 2J_GK - J_GPD - V_I * kG3P * G3P,
+        (V_I + V_MTX) * D(Pyr) ~ (J_GPD - J_PDH - J_LDH) - (V_I + V_MTX) * kPyr * Pyr,
+        V_I * D(ATP_c) ~ (-gk_atp_stoich * J_GK + 2 * J_GPD + J_ANT + J_ADK) - V_I * ATP_c * (kATP + kATPCa * Ca_c),
+        V_I * D(AMP_c) ~ J_ADK,
         # D(ADP_c) ~ # Conserved
         # D(x[1]) ~ # Conserved
         D(x[2]) ~ v[1] - v[2],
