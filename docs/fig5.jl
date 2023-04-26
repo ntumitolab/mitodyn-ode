bc97669c66e46c73c39b2440b6195ab8b4149817
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-04-26 12:48:47 +0800

    ANT and HL

--- docs/fig5.jl
+++ docs/fig5.jl
@@ -43,19 +43,16 @@ prob = ODEProblem(sysosci, sssol.u, tend, [GlcConst => 10])
 sol = solve(prob, saveat=ts)
 
 #---
-
 function plot_fig5(sol, figsize=(10, 10))
     ts = sol.t
     tsm = ts ./ 60
-    @unpack Ca_c, Ca_m, G3P, Pyr, NADH_c, NADH_m, ATP_c, ADP_c, ΔΨm, degavg = sys
-    ## TODO: panel B and C not needed
-    fig, ax = plt.subplots(4, 1; figsize)
+    @unpack Ca_c, Ca_m, ATP_c, ADP_c, ΔΨm, degavg, J_ANT, J_HL = sys
+    fig, ax = plt.subplots(5, 1; figsize)
 
     ax[1].plot(tsm, sol[Ca_c * 1000], label="Cyto. Ca (μM)")
     ax[1].plot(tsm, sol[Ca_m * 1000], label="Mito. Ca (μM)")
     ax[1].set_title("A", loc="left")
 
-    ## TODO: describe state III behavior
     ax[2].plot(tsm, sol[ATP_c / ADP_c], label="ATP:ADP")
     ax[2].set_title("B", loc="left")
 
@@ -64,7 +61,11 @@ function plot_fig5(sol, figsize=(10, 10))
 
     ax[4].plot(tsm, sol[degavg], label="<k>")
     ax[4].set_title("D", loc="left")
-    ax[4].set(xlabel="Time (minute)")
+
+    ax[5].plot(tsm, sol[J_ANT], label="ANT (mM/s)")
+    ax[5].plot(tsm, sol[J_HL], label="H leak (mM/s)")
+    ax[5].set_title("E", loc="left")
+    ax[5].set(xlabel="Time (minute)")
 
     for a in ax
         a.grid()
