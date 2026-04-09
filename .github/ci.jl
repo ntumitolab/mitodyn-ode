c9ef4e8c487f19f72b0758cbcef4e8167d932af6
Wen Wei Tseng <sosiristseng@gmail.com>
2026-04-09 20:34:57 +0800

    CI

--- .github/ci.jl
+++ .github/ci.jl
@@ -1,17 +1,23 @@
-using Distributed
-using Tables
-using MarkdownTables
-using SHA
+using JSON
+using Literate
 
-@everywhere begin
-    ENV["GKSwstype"] = "100"
-    using Literate, JSON
+ENV["GKSwstype"] = "100"
+
+function main(; rmsvg=true)
+    file = get(ENV, "NB", "test.ipynb")
+    cachedir = get(ENV, "NBCACHE", ".cache")
+    if endswith(file, ".jl")
+        run_literate(file; cachedir, rmsvg)
+    elseif endswith(file, ".ipynb")
+        lit = to_literate(file)
+        run_literate(lit; cachedir, rmsvg)
+    end
 end
 
 # Strip SVG output from a Jupyter notebook
-@everywhere function strip_svg(nbpath)
-    oldfilesize = filesize(nbpath)
-    nb = open(JSON.parse, nbpath, "r")
+function strip_svg(ipynb)
+    oldfilesize = filesize(ipynb)
+    nb = open(JSON.parse, ipynb, "r")
     for cell in nb["cells"]
         !haskey(cell, "outputs") && continue
         for output in cell["outputs"]
@@ -23,30 +29,10 @@ end
             end
         end
     end
-    rm(nbpath; force=true)
-    write(nbpath, JSON.json(nb, 1))
-    @info "Stripped SVG in $(nbpath). The original size is $(oldfilesize). The new size is $(filesize(nbpath))."
-    return nbpath
-end
-
-# Remove cached notebook and sha files if there is no corresponding notebook
-function clean_cache(cachedir)
-    for (root, _, files) in walkdir(cachedir)
-        for file in files
-            fn, ext = splitext(file)
-            if ext == ".sha"
-                target = joinpath(joinpath(splitpath(root)[2:end]), fn)
-                nb = target * ".ipynb"
-                lit = target * ".jl"
-                if !isfile(nb) && !isfile(lit)
-                    cachepath = joinpath(root, fn)
-                    @info "Notebook $(nb) or $(lit) not found. Removing $(cachepath) SHA and notebook."
-                    rm(cachepath * ".sha")
-                    rm(cachepath * ".ipynb"; force=true)
-                end
-            end
-        end
-    end
+    rm(ipynb; force=true)
+    write(ipynb, JSON.json(nb, 2))
+    @info "The original size is $(Base.format_bytes(oldfilesize)). The new size is $(Base.format_bytes(filesize(ipynb)))."
+    return ipynb
 end
 
 # Convert a Jupyter notebook into a Literate notebook. Adapted from https://github.com/JuliaInterop/NBInclude.jl.
@@ -72,39 +58,7 @@ function to_literate(nbpath; shell_or_help = r"^\s*[;?]")
     return jlpath
 end
 
-# List notebooks without caches in a file tree
-function list_notebooks(basedir, cachedir)
-    list = String[]
-    for (root, _, files) in walkdir(basedir)
-        for file in files
-            name, ext = splitext(file)
-            if ext == ".ipynb" || ext == ".jl"
-                nb = joinpath(root, file)
-                shaval = read(nb, String) |> sha256 |> bytes2hex
-                @info "$(nb) SHA256 = $(shaval)"
-                shafilename = joinpath(cachedir, root, name * ".sha")
-                if isfile(shafilename) && read(shafilename, String) == shaval
-                    @info "$(nb) cache hits and will not be executed."
-                else
-                    @info "$(nb) cache misses. Writing hash to $(shafilename)."
-                    mkpath(dirname(shafilename))
-                    write(shafilename, shaval)
-                    if ext == ".ipynb"
-                        litnb = to_literate(nb)
-                        rm(nb; force=true)
-                        push!(list, litnb)
-                    elseif ext == ".jl"
-                        push!(list, nb)
-                    end
-                end
-            end
-        end
-    end
-    return list
-end
-
-# Run a Literate notebook
-@everywhere function run_literate(file, cachedir; rmsvg=true)
+function run_literate(file; cachedir = ".cache", rmsvg = true)
     outpath = joinpath(abspath(pwd()), cachedir, dirname(file))
     mkpath(outpath)
     ipynb = Literate.notebook(file, dirname(file); mdstrings=true, execute=true)
@@ -113,39 +67,4 @@ end
     return ipynb
 end
 
-function main(;
-    basedir=get(ENV, "DOCDIR", "docs"),
-    cachedir=get(ENV, "NBCACHE", ".cache"),
-    rmsvg=true)
-
-    mkpath(cachedir)
-    clean_cache(cachedir)
-    litnbs = list_notebooks(basedir, cachedir)
-
-    if !isempty(litnbs)
-        # Execute literate notebooks in worker process(es)
-        ts_lit = pmap(litnbs; on_error=identity) do nb
-            @elapsed run_literate(nb, cachedir; rmsvg)
-        end
-        rmprocs(workers()) # Remove worker processes to release some memory
-        failed = false
-        for (nb, t) in zip(litnbs, ts_lit)
-            if t isa ErrorException
-                println("Notebook: ", nb, "failed with error: \n", t.msg)
-                failed = true
-            end
-        end
-
-        if failed
-            error("Please check literate notebook error(s).")
-        end
-    else
-        ts_lit = []
-    end
-
-    # Print execution result
-    Tables.table([litnbs ts_lit]; header=["Notebook", "Elapsed (s)"]) |> markdown_table(String) |> print
-end
-
-# Run code
 main()
