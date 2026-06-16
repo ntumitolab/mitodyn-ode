eec3e23d137552fd823887fc88ca85e9a61bbb7e
Wen Wei Tseng <sosiristseng@gmail.com>
2026-06-17 00:30:11 +0800

    seperate fiss and fuse params

--- src/MitochondrialDynamics.jl
+++ src/MitochondrialDynamics.jl
@@ -8,7 +8,7 @@ export make_model, indexof, extract, exportTIF
 include("utils.jl")
 
 "Average cytosolic calcium level based on the ATP:ADP ratio"
-function cac_atp(; ca_base = 0.09μM, ca_act = 0.25μM, n=4, katp=25)
+function cac_atp(; ca_base=0.09μM, ca_act=0.25μM, n=4, katp=25)
     @independent_variables t
     @variables Ca_c(t) ATP_c(t) ADP_c(t)
     @parameters (RestingCa=ca_base, ActivatedCa=ca_act, NCac=n, KatpCac=katp)
@@ -25,7 +25,7 @@ function make_model(;
     D = Differential(t)
 
     @constants begin
-        C_MIT=1.812μM/mV # Mitochondrial membrane capacitance
+        C_MIT = 1.812μM / mV # Mitochondrial membrane capacitance
         F_M = 3E-4       # Fraction of free Ca in mitochondria
         F_I = 0.01       # Fraction of free Ca in cytosol
         V_I = 0.53       # Relative cytoplasmic volume
@@ -34,28 +34,28 @@ function make_model(;
     end
 
     # Adjustable parameters
-    @parameters (rETC = 1, rHL = 1, rF1 = 1, rPDH = 1)
+    @parameters (rETC=1, rHL=1, rF1=1, rPDH=1)
 
     # Adenylate kinase (AdK)
-    @variables AEC(t)= 0.9 ATP_c(t) ADP_c(t) AMP_c(t)
-    @parameters (kfAK=1000Hz/mM, kEqAK=0.931)
+    @variables AEC(t) = 0.9 ATP_c(t) ADP_c(t) AMP_c(t)
+    @parameters (kfAK=1000Hz / mM, kEqAK=0.931)
 
     # Glucokinase (GK)
     @variables J_GK(t)
     @parameters Glc = 5mM
-    @parameters (VmaxGK=0.011mM*Hz, KatpGK=0.5mM, KglcGK=7mM, nGK=1.7, ATPstiochGK=2)
+    @parameters (VmaxGK=0.011mM * Hz, KatpGK=0.5mM, KglcGK=7mM, nGK=1.7, ATPstiochGK=2)
 
     # Glyceraldehydes 3-phosphate dehydrogenase (GPD)
-    @variables J_GPD(t) G3P(t)= 2.9μM NAD_c(t) NADH_c(t)= 1μM
-    @parameters (VmaxGPD=0.5mM*Hz, Kg3pGPD=0.2mM, KnadGPD=0.09, KadpGPD=2μM)
+    @variables J_GPD(t) G3P(t) = 2.9μM NAD_c(t) NADH_c(t) = 1μM
+    @parameters (VmaxGPD=0.5mM * Hz, Kg3pGPD=0.2mM, KnadGPD=0.09, KadpGPD=2μM)
 
     # Lactate dehydrogenase (LDH)
-    @variables J_LDH(t) Pyr(t)= 8.7μM
-    @parameters (VmaxLDH=1.2mM*Hz, KpyrLDH=47.5μM, KnadhLDH=1)
+    @variables J_LDH(t) Pyr(t) = 8.7μM
+    @parameters (VmaxLDH=1.2mM * Hz, KpyrLDH=47.5μM, KnadhLDH=1)
 
     # Pyruvate dehydrogenase (PDH)
-    @variables J_PDH(t) J_DH(t) NAD_m(t) NADH_m(t)= 57μM Ca_m(t)= 0.200μM J_FFA(t)
-    @parameters (VmaxPDH=300μM*Hz, KpyrPDH=47.5μM, KnadPDH=81, U1PDH=1.5, U2PDH=1.1, KcaPDH=0.05μM, kFFA=0Hz)
+    @variables J_PDH(t) J_DH(t) NAD_m(t) NADH_m(t) = 57μM Ca_m(t) = 0.200μM J_FFA(t)
+    @parameters (VmaxPDH=300μM * Hz, KpyrPDH=47.5μM, KnadPDH=81, U1PDH=1.5, U2PDH=1.1, KcaPDH=0.05μM, kFFA=0Hz)
     pdheq = let
         c = (hil(KcaPDH, Ca_m))^2
         fpCa = hil(1, U2PDH * (1 + U1PDH * c))
@@ -65,20 +65,20 @@ function make_model(;
     end
 
     # Electron transport chain (ETC)
-    @variables J_HR(t) J_O2(t) ΔΨm(t)= 92mV
-    @parameters (VmaxETC=22mM*Hz, KnadhETC=3mM, KaETC=-4.92E-3/mV, KbETC=-4.43E-3/mV)
+    @variables J_HR(t) J_O2(t) ΔΨm(t) = 92mV
+    @parameters (VmaxETC=22mM * Hz, KnadhETC=3mM, KaETC=-4.92E-3 / mV, KbETC=-4.43E-3 / mV)
 
     # Proton leak
     @variables J_HL(t)
-    @parameters (pHleak=2.4μM*Hz, kvHleak=0.0305/mV)
+    @parameters (pHleak=2.4μM * Hz, kvHleak=0.0305 / mV)
 
     # F1Fo ATPase (ATP synthase) lumped with ANT
     @variables J_HF(t) J_ANT(t)
-    @parameters (VmaxF1=8mM*Hz, KadpF1=20μM, KvF1=131.4mV, KcaF1=0.165μM, FmgadpF1=0.055, nadpF1=2, nvF1=8)
+    @parameters (VmaxF1=8mM * Hz, KadpF1=20μM, KvF1=131.4mV, KcaF1=0.165μM, FmgadpF1=0.055, nadpF1=2, nvF1=8)
 
     # Mitochondrial calcium uniporter (MCU)
     @variables J_MCU(t) Ca_c(t)
-    @parameters PcaMCU=4Hz
+    @parameters PcaMCU = 4Hz
     mcueq = let
         zvfrt = 2 * iVT * ΔΨm
         em1 = expm1(zvfrt)
@@ -87,7 +87,7 @@ function make_model(;
 
     # Mitochondrial sodium-calcium exchanger (NCLX)
     @variables J_NCLX(t)
-    @parameters (Na_c=10mM, Na_m=5mM, VmaxNCLX=75μM*Hz, KnaNCLX=8.2mM, KcaNCLX=8μM)
+    @parameters (Na_c=10mM, Na_m=5mM, VmaxNCLX=75μM * Hz, KnaNCLX=8.2mM, KcaNCLX=8μM)
     nclxeq = let
         A = (Na_c / KnaNCLX)^2
         P = (Na_m / KnaNCLX)^2
@@ -100,16 +100,16 @@ function make_model(;
 
     # NADH shuttle
     @variables J_NADHT(t)
-    @parameters (VmaxNADHT=50μM*Hz, Ktn_c=0.002, Ktn_m=16.78)
+    @parameters (VmaxNADHT=50μM * Hz, Ktn_c=0.002, Ktn_m=16.78)
 
     # Baseline consumption rates
-    @parameters (kNADHc=0.1Hz, kNADHm=0.1Hz, kATP=0.04Hz, kATPCa=90Hz/mM, kG3P=0.01Hz, kPyr=0.01Hz)
+    @parameters (kNADHc=0.1Hz, kNADHm=0.1Hz, kATP=0.04Hz, kATPCa=90Hz / mM, kG3P=0.01Hz, kPyr=0.01Hz)
     # Conservation relationships
     @parameters (ΣAc=4.56mM, Σn_c=2mM, Σn_m=2.2mM)
 
     # Fission-fusion rates
-    @variables x1(t) x2(t)= 0.24 x3(t)=0.06 degavg(t) tiptip(t) tipside(t)
-    @parameters (kfiss1=inv(10minute), kfuse1=kfiss1, kfiss2=1.5*kfiss1, kfuse2=0.5*kfuse1)
+    @variables x1(t) x2(t) = 0.24 x3(t) = 0.06 degavg(t) tiptip(t) tipside(t)
+    @parameters (kfiss1=inv(10minute), kfuse1=inv(10minute), kfiss2=1.5 * inv(10minute), kfuse2=0.5 * inv(10minute))
 
     eqs = [
         caceq,
@@ -146,7 +146,7 @@ function make_model(;
         D(ΔΨm) ~ inv(C_MIT) * (J_HR - J_HF - J_HL - J_ANT - 2 * J_MCU),
         D(G3P) ~ inv(V_I) * (2J_GK - J_GPD) - kG3P * G3P,
         D(Pyr) ~ inv(V_I + V_MTX) * (J_GPD - J_PDH - J_LDH) - kPyr * Pyr,
-        D(AEC) ~ (inv(V_I) * (-ATPstiochGK * J_GK + 2 * J_GPD + J_ANT) - ATP_c * (kATP + kATPCa * Ca_c))/ΣAc,
+        D(AEC) ~ (inv(V_I) * (-ATPstiochGK * J_GK + 2 * J_GPD + J_ANT) - ATP_c * (kATP + kATPCa * Ca_c)) / ΣAc,
         # D(x[1]) ~ # Conserved
         D(x2) ~ tiptip - tipside,
         D(x3) ~ tipside,
